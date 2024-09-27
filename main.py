f5294ee934c316ba6eff57b39f36c869b096a404
lanigaines <lanigaines@live.com>
2024-09-26 18:39:21 -0700

    add mainpy file

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    ##################################################
+    # Comlete your code here
+    ##################################################
+    """
+    Use following variables to save your result
+    total 
+    average
+    """
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return total, average
+
+
+if __name__ == '__main__':
+    main()
